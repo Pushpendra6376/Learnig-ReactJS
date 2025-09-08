35bf5370bafa9a005f5d8f4dbf82a044260fad77
pushpendra6376 <pp5395021@gmail.com>
2025-09-08 21:32:28 +0530

    learned how to create components

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,31 @@ function App(){
       <Header />
       <h1>Hello world!</h1>
       <p>this is me.</p>
+      <Fruits></Fruits>
+      <Colours />
+    </div>
+  )
+}
 
+function Fruits(){
+  return (
+    <div>
+      <ul>
+        <li>Apple</li>
+        <li>Banana</li>
+        <li>Kiwi</li>
+        <li>Lichi</li>
+      </ul>
+    </div>
+  )
+}
+
+function Colours(){
+  return (
+    <div>
+      <ul>
+        <li>Blue</li>
+      </ul>
     </div>
   )
 }
