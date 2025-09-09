64be10d33b0d1271236fba464765982e10e5e328
pushpendra6376 <pp5395021@gmail.com>
2025-09-09 12:13:48 +0530

    deleted some loines of code

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,14 @@
-import Header from "./Header";
+
 
 function App(){
   return (
     <div>
-      <Header />
       <h1>Hello world!</h1>
       <p>this is me.</p>
-      <Fruits></Fruits>
-      <Colours />
+
     </div>
   )
 }
 
-function Fruits(){
-  return (
-    <div>
-      <ul>
-        <li>Apple</li>
-        <li>Banana</li>
-        <li>Kiwi</li>
-        <li>Lichi</li>
-      </ul>
-    </div>
-  )
-}
-
-function Colours(){
-  return (
-    <div>
-      <ul>
-        <li>Blue</li>
-      </ul>
-    </div>
-  )
-}
 
 export default App;
